64911b72ae5f319e79eb64e918cf857722387499
Olga <olgaharceva946@gmail.com>
2022-08-15 16:41:55 +0300

    add Задача 3

---
+++ Ex003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string messege)
+{
+    Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+
+int value = Prompt("Введите число: ");
+if (value > 99)
+{
+    while (value > 100)
+    {
+        value = value / 10;
+    }
+    int result = value % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Нет третьей цифры ");
+}
