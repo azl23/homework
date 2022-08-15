32d66a6792e52f5c02b59db16d604b69bd2b26a8
Olga <olgaharceva946@gmail.com>
2022-08-15 16:38:40 +0300

    add дз

---
+++ Exx01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+
+int Prompt(string messege)
+{
+    Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+
+int number = Prompt("Введите трехзначное число ");
+if (number >= 100 && number < 1000)
+{
+    int result = number / 10 % 10;
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Число не трехзначное! ");
+}
